f4ad5cf55d4784c2fd93bcf3eff64b1e0f58d681
Svitlana Koshchii <Svitlana.Koshchii@teaminternational.com>
2019-11-26 16:26:50 +0200

    Add project files.

---
+++ OOP_SquareTask/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace OOP_SquareTask
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			CalculateRoundSquare();
+			CalculateAreaOfSquare();
+
+			static void CalculateRoundSquare()
+			{
+				const double pi = 3.14; // pi 
+
+				Console.WriteLine("Enter ROUND radius: ");
+				double radius = Convert.ToDouble(Console.ReadLine()); // radius
+				double roundSquare = pi * radius * 2; // round square calculation
+				Console.WriteLine("ROUND square equals to {0}", roundSquare);
+
+			}
+
+			static void CalculateAreaOfSquare()
+			{
+
+				Console.WriteLine("Enter SQUARE side : ");
+				double side = Convert.ToDouble(Console.ReadLine()); // side of square
+				double areaOfSquare = side * side; // area of square calculation
+				Console.WriteLine("Area of SQAURE  equals to {0}", areaOfSquare);
+			}
+
+		}
+	}
+}
