3a5fecc17ae52bc45c37cbb707e347053e9173f4
Svetlana Koshchy <svetlana.koshchy@gmail.com>
2019-11-27 13:08:29 +0200

    add validation of entered values for radius and side

--- OOP_SquareTask/Program.cs
+++ OOP_SquareTask/Program.cs
@@ -2,33 +2,48 @@
 
 namespace OOP_SquareTask
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			CalculateRoundSquare();
-			CalculateAreaOfSquare();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculateRoundSquare();
+            CalculateSquareArea();
 
-			static void CalculateRoundSquare()
+            static void CalculateRoundSquare()
 			{
 				const double pi = 3.14; // pi 
-
-				Console.WriteLine("Enter ROUND radius: ");
-				double radius = Convert.ToDouble(Console.ReadLine()); // radius
-				double roundSquare = pi * Math.Pow(radius,2); // round square calculation
-				Console.WriteLine("ROUND square equals to {0}", roundSquare);
-
+                double radius; // result side 
+                Console.WriteLine("Enter ROUND radius: ");
+				string radiusInput = Console.ReadLine(); // user input value for radius
+                if (double.TryParse(radiusInput, out radius))
+                {
+                    double roundSquare = pi * Math.Pow(radius, 2); // round square calculation
+                    Console.WriteLine("ROUND square equals to {0}", roundSquare);
+                }
+                else
+                {
+                    Console.WriteLine("Enter SQUARE side validation failed");
+                }
 			}
 
-			static void CalculateAreaOfSquare()
-			{
+            static void CalculateSquareArea()
+            {
 
-				Console.WriteLine("Enter SQUARE side : ");
-				double side = Convert.ToDouble(Console.ReadLine()); // side of square
-				double areaOfSquare = Math.Pow(side,2); // area of square calculation
-				Console.WriteLine("Area of SQAURE  equals to {0}", areaOfSquare);
-			}
+                double side; // result side 
+                Console.WriteLine("Enter SQUARE side : ");
+                string sideInput = Console.ReadLine(); // user input value for side of square 
 
-		}
-	}
+                if (double.TryParse(sideInput, out side)) // check if userr input is double
+                {
+                    double areaOfSquare = Math.Pow(side, 2); // area of square calculation
+                    Console.WriteLine("Area of SQAURE  equals to {0}", areaOfSquare);
+                }
+                else
+                {
+                    Console.WriteLine("Enter SQUARE side validation failed");
+                }
+            }
+
+        }
+    }
 }
