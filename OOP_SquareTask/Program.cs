622ec1487a3bca2bb53a5900570e9c4dc94644f5
Gleb Gamaun <gvg3777@gmail.com>
2019-11-27 23:36:09 +0200

    calculation of round square is moved to separate custom metod, random

--- OOP_SquareTask/Program.cs
+++ OOP_SquareTask/Program.cs
@@ -4,27 +4,70 @@ namespace OOP_SquareTask
 {
     class Program
     {
+        const double pi = 3.14; // pi
+        const double minRandomBorder = 0.5; // min random border
+        const double maxRandomBorder = 5; // max random border
+        private static double roundSquare;
+
+        // public int roundInputAttempt; //user input radius attempt
+
         static void Main(string[] args)
         {
             CalculateRoundSquare();
-            CalculateSquareArea();
+            // CalculateSquareArea();
+
+            Program program = new Program();
 
             static void CalculateRoundSquare()
 			{
-				const double pi = 3.14; // pi 
+                Random random = new Random();                
+               
+                int roundInputAttempt;
                 double radius; // result side 
-                Console.WriteLine("Enter ROUND radius: ");
-				string radiusInput = Console.ReadLine(); // user input value for radius
-                if (double.TryParse(radiusInput, out radius))
+
+                for (roundInputAttempt = 1; roundInputAttempt < 4; roundInputAttempt++)
                 {
-                    double roundSquare = pi * Math.Pow(radius, 2); // round square calculation
-                    Console.WriteLine("ROUND square equals to {0}", roundSquare);
+                    Console.WriteLine("Enter ROUND radius: ");
+                    string radiusInput = Console.ReadLine(); // user input value for radius
+
+                    if (double.TryParse(radiusInput, out radius))
+                    {
+                        CalculateSquareFormlula(radius);  // round square calculation
+                        
+                        break;
+                    }
+                    else if (roundInputAttempt < 3)
+                    {
+                        Console.WriteLine("Wrong attempt {0}. Please, enter only numbers.", roundInputAttempt);
+                            
+                    }
+                    else
+                    {
+                        Console.WriteLine("Last attempt is reached. Radius is selected randomly.");
+
+                        radius = random.NextDouble() * (maxRandomBorder - minRandomBorder) + minRandomBorder;
+                        radius = GetRoundedNumber(radius);
+                        Console.WriteLine("Random radius = {0}", radius);
+
+                        CalculateSquareFormlula(radius);  // round square calculation
+                        
+                    }
+                    
                 }
-                else
-                {
-                    Console.WriteLine("Enter SQUARE side validation failed");
-                }
-			}
+               
+            }
+
+            static void CalculateSquareFormlula(double radius)
+            {
+                double roundSquare = pi * Math.Pow(Math.Round(radius, 2), 2); // round square calculation formula
+                roundSquare = GetRoundedNumber(roundSquare);
+                Console.WriteLine("ROUND square equals to {0}", roundSquare);  
+            }
+
+            static double GetRoundedNumber(double number)
+            {
+                return Math.Round(number, 2);
+            }
 
             static void CalculateSquareArea()
             {
