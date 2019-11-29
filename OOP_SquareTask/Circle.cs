15dcd6a616fa9f3ff22ac3d8ce66e04aa58e9d92
Svetlana Koshchy <svetlana.koshchy@gmail.com>
2019-11-29 14:51:39 +0200

    add classes for Circle and Square

---
+++ OOP_SquareTask/Circle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_SquareTask
+{
+    class Circle
+    {
+        public double radius;
+
+        //Consctructor with side parameter
+        public Circle(double r)
+        {
+            radius = r;
+        }
+
+        //method calculate area of square 
+        public void CalculateSquareArea() { }
+
+        //property Square
+        public double Side
+        {
+            get
+            {
+                return radius;
+            }
+
+            set // add validatation that input value for radius is double
+            {
+                radius = value;
+            }
+        }
+    }
+}
+
+
+
+
---
+++ OOP_SquareTask/Square.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_SquareTask
+{
+    class Square
+    {
+        public double side;
+
+        //Consctructor with side parameter
+        public Square(double s)
+        {
+            side = s;
+        }
+
+        //method calculate area of square 
+        public void CalculateSquareArea() { }
+
+        //property Square
+        public double Side
+        {
+            get
+            {
+                return side;
+            }
+
+            set // add validatation that input value for side is double
+            {
+                side = value;
+            }
+        }
+    }
+}
+
+
