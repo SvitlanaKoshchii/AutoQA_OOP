d32cc82033f14c469a4a7dfd9e4e4f6ebf321be9
Gleb Gamaun <gvg3777@gmail.com>
2019-12-02 22:25:01 +0200

    add Constants class

---
+++ OOP_SquareTask/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace OOP_SquareTask
+{
+    static class Constants
+    {
+        public const double minRandomBorder = 0.5; // min random border
+        public const double maxRandomBorder = 5; // max random border
+        public const int roundingNumber = 2; // rounded entered value after point
+
+    }
+}
