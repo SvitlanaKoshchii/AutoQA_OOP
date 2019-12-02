ba0d471037ca6862a54839c70a80fea0421bea2a
Gleb Gamaun <gvg3777@gmail.com>
2019-12-02 22:45:04 +0200

    ValidationHelper class added, add double validation methods.

---
+++ OOP_SquareTask/ValidationHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace OOP_SquareTask
+{
+    public class ValidationHelper
+    {
+        public ValidationHelper()
+        {
+        }
+
+        public bool IsDouble(string enteredSide)
+        {
+            if (double.TryParse(enteredSide, out double side)) // check if userr input is double
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public double GetValidatedDouble(string side)
+        {
+            return double.Parse(side.Replace(",", "."));
+        }
+
+    }
+}
