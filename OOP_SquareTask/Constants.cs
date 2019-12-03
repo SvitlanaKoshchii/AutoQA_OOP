175dc0361714b55aeca52a94d8ce35b61e742fa1
Gleb Gamaun <gvg3777@gmail.com>
2019-12-03 23:47:23 +0200

    New constants were added

--- OOP_SquareTask/Constants.cs
+++ OOP_SquareTask/Constants.cs
@@ -5,6 +5,7 @@ namespace OOP_SquareTask
     {
         public const double minRandomBorder = 0.5; // min random border
         public const double maxRandomBorder = 5; // max random border
+        public const double wrongDouble = -1; // if user write not correct value
 
         public const int roundingNumber = 2; // rounded entered value after point
         public const int maxAttempt = 3; //max invalid attempt
@@ -12,7 +13,8 @@ namespace OOP_SquareTask
         public const string enterSquareSide = "Enter SQUARE side : ";
         public const string enterCircleRadius = "Enter CIRCLE radius : ";
         public const string showSquareArea = "Square Area = ";
+        public const string showCircleArea = "Circle Area = ";
         public const string showErrorInputValue = "Invalid input. Please, enter number which can contain dot or coma.";
-        public const string showAttemptsEnded = "Last attempt is reached. Radius is selected randomly.";
+        public const string showAttemptsEnded = "Last attempt is reached. Value is selected randomly.";
     }
 }
