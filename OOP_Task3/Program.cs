090350eb894954429f835b5df4ad2edcf9045490
Svetlana Koshchy <svetlana.koshchy@gmail.com>
2019-12-04 17:57:15 +0200

    Task3 project is added.

---
+++ OOP_Task3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace OOP_Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {     
+            ArrayList myList = new ArrayList();
+
+            int rangeStart;
+            int rangeEnd;
+            int sum = 0;
+
+            //set start of array
+            Console.WriteLine("Enter a start of range");
+            rangeStart = Int32.Parse(Console.ReadLine());
+
+            //set end of array
+            Console.WriteLine("Enter a end of range");
+            rangeEnd = Int32.Parse(Console.ReadLine());
+
+            //add elements to array depending on start and end range value
+            for (int i = rangeStart; i <= rangeEnd; i++) //end of range is wrong!!!! Need to fix
+            {
+                myList.Add(i);
+            }
+
+            // Displaying the elements in ArrayList 
+            Console.WriteLine("View the array elements:");
+            foreach (int i in myList)
+            {
+                Console.WriteLine(i);
+            }
+
+            //count sum (currentry does not includes last element of array. Need to fix)
+            foreach (int i in myList)
+            {
+                if (i % 3 == 0 && i % 5 != 0) 
+                {
+                    sum = sum + i;
+                }
+            
+            }
+            Console.WriteLine("Sum is {0}", sum);
+
+            Console.ReadKey();
+
+        }
+    }
+}
