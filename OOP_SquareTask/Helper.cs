11af71e4995c1df44a88dd1ba9985c164a613411
Svetlana Koshchy <svetlana.koshchy@gmail.com>
2019-12-11 15:18:22 +0200

    New validation method which can be reused, adding method to show results, adding method to check positive values

--- OOP_SquareTask/Helper.cs
+++ OOP_SquareTask/Helper.cs
@@ -5,29 +5,33 @@ namespace OOP_Task1_2_CircleSquareCalculations
 {
     public class Helper
     {
-        public double GetDouble(string enteredSide) //method to check if entered value is double 
-        {
-            string replacedComa = enteredSide.Replace(",", ".");
-            double result;
 
-            // Try parsing in the current culture
-            if (!double.TryParse(replacedComa, System.Globalization.NumberStyles.Any, CultureInfo.CurrentCulture, out result) &&
-                // Then try in US english
-                !double.TryParse(replacedComa, System.Globalization.NumberStyles.Any, CultureInfo.GetCultureInfo("en-US"), out result) &&
-                // Then in neutral language
-                !double.TryParse(replacedComa, System.Globalization.NumberStyles.Any, CultureInfo.InvariantCulture, out result))
+        public double GetValidDoubleValue(string valueType, string figure)
+        {
+            for (int i = 1; i <= Constants.maxAttempt; i++)
             {
-                result = Constants.wrongDouble; 
+                Console.WriteLine(Constants.showEnterValue, valueType, figure);
+
+                string inputVal = Console.ReadLine().Replace(",", ".");
+                bool isDouble = double.TryParse(inputVal, out double result);
+
+                if (isDouble && isPositive(result))
+                {
+                    return GetRoundedNumber(result);
+                }
+                else
+                {
+                    Console.WriteLine(Constants.showErrorInputValue, i, Constants.maxAttempt);
+                }
             }
-            return result;
+            return GetRandomValue();
         }
 
-        public double ReplaceComaToDot(string side) // method to replace coma to dot in user input 
+        public void showAreaResult(string figure, double result)
         {
-            string replacedComa = side.Replace(",", ".");
-
-            return double.Parse(replacedComa, CultureInfo.InvariantCulture);
+            Console.WriteLine(Constants.showAreaResult, figure, GetRoundedNumber(result));
         }
+
 
         public double GetRoundedNumber(double number) //method to round value up to specific to the particular number of fractional digits
         {
@@ -48,6 +52,61 @@ namespace OOP_Task1_2_CircleSquareCalculations
             return value;
         }
 
+        public bool isPositive(double value)
+        {
+            if (value > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void showFiguresPlaceEachOther(double circleArea, double squareArea)
+        {
+            if (2 * Math.Sqrt(circleArea / Math.PI) <= Math.Sqrt(squareArea))
+            {
+                Console.WriteLine(Constants.showCirclePlacedSquare);
+            }
+            else if (2 * Math.Sqrt(circleArea / Math.PI) >= Math.Sqrt(2) * Math.Sqrt(squareArea))
+            {
+                Console.WriteLine(Constants.showSquarePlacedCircle);
+            }
+            else Console.WriteLine(Constants.showSquareAndCircleCanNotPlace);
+
+        }
+
+        
+
+
+        /*public double GetDouble(string enteredSide) //method to check if entered value is double 
+        {
+            string replacedComa = enteredSide.Replace(",", ".");
+            double result;
+
+            // Try parsing in the current culture
+            if (!double.TryParse(replacedComa, System.Globalization.NumberStyles.Any, CultureInfo.CurrentCulture, out result) &&
+                // Then try in US english
+                !double.TryParse(replacedComa, System.Globalization.NumberStyles.Any, CultureInfo.GetCultureInfo("en-US"), out result) &&
+                // Then in neutral language
+                !double.TryParse(replacedComa, System.Globalization.NumberStyles.Any, CultureInfo.InvariantCulture, out result))
+            {
+                result = Constants.wrongDouble;
+            }
+            return result;
+        }
+
+        public double ReplaceComaToDot(string side) // method to replace coma to dot in user input 
+        {
+            string replacedComa = side.Replace(",", ".");
+
+            return double.Parse(replacedComa, CultureInfo.InvariantCulture);
+        }
+
+        
+
         public double GetDoubleFromString(string value) // method to retrieve double from entered string
         {
             double parsedDouble = GetDouble(value);
@@ -61,8 +120,8 @@ namespace OOP_Task1_2_CircleSquareCalculations
                 Console.WriteLine(Constants.showErrorInputValue);
 
                 return Constants.wrongDouble;
-            }
-        }
+            }*/
     }
+
 }
 
