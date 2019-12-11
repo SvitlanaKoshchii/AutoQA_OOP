c294bf50fcdc49e9ff119254f308b41af7d80b53
Svetlana Koshchy <svetlana.koshchy@gmail.com>
2019-12-11 15:16:58 +0200

    updates to existes constants and add new constants

--- OOP_SquareTask/Constants.cs
+++ OOP_SquareTask/Constants.cs
@@ -5,24 +5,30 @@ namespace OOP_Task1_2_CircleSquareCalculations
     {
         public const double minRandomBorder = 0.5; // min random border
         public const double maxRandomBorder = 5; // max random border
-        public const double wrongDouble = -1.0; // if user write not correct value
+
+        //public const double wrongDouble = -1.0; // if user write not correct value
 
         public const int roundingNumber = 2; // rounded entered value after point
         public const int maxAttempt = 3; //max invalid attempt
 
-        public const string enterSquareSide = "Enter SQUARE side : ";
-        public const string enterCircleRadius = "Enter CIRCLE radius : ";
-        public const string showSquareArea = "Square Area = ";
-        public const string showCircleArea = "Circle Area = ";
-        public const string showErrorInputValue = "Invalid input. Value should be numerical, positive and can contain dot or coma.";     
+        public const string showEnterValue = "Enter {0} of {1}";
+        public const string showAreaResult = "Area of {0} equals to: {1}";
+
+        public const string showErrorInputValue = "Invalid input. Value should be numerical, positive and can contain dot or coma. Attempt {0} of {1}";
         public const string showAttemptsEnded = "Last attempt is reached. Value is selected randomly.";
 
-        public const string showSquarePlacedCircle = "Square is placed in Circle.";
-        //public const string showSquareLessCircle = "Square is placed in Circle and doesn't touch the borders.";
-        public const string showSquareNotPlacedCircle = "The Square is NOT placed in the Circle.";
+        //public const string enterSquareSide = "Enter SQUARE side : ";
+        //public const string enterCircleRadius = "Enter CIRCLE radius : ";
+        //public const string showSquareArea = "Square Area = ";
+        //public const string showCircleArea = "Circle Area = ";      
 
-        public const string showCirclePlacedSquare = "Circle is placed in the Square.";
-        //public const string showCircleLessSquare = "Circle is placed in Square and doesn't touch the borders.";
-        public const string showCircleNotPlacedSquare = "Circle is NOT placed in Square.";
+        public const string showSquarePlacedCircle = "SQUARE is placed in CIRCLE. CIRCLE can't be places in SQUARE";
+
+        public const string showSquareNotPlacedCircle = "SQUARE is NOT placed in the CIRCLE.";
+
+        public const string showCirclePlacedSquare = "CIRCLE is placed in the SQUARE. SQUARE can't be placed in CIRCLE";
+        public const string showCircleNotPlacedSquare = "CIRCLE is NOT placed in SQUARE.";
+
+        public const string showSquareAndCircleCanNotPlace = "Neither SQUARE can place in CIRCLE nor CIRCLE can place in SQUARE";
     }
 }
