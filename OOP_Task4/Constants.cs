664434734afd4d170b28e6a364d2f24798bf8cc8
Svetlana Koshchy <svetlana.koshchy@gmail.com>
2019-12-12 15:14:15 +0200

    minor updates to constants

--- OOP_Task4/Constants.cs
+++ OOP_Task4/Constants.cs
@@ -7,7 +7,8 @@ namespace OOP_Task4
     static class Constants
     {
         public const string showEnterWord = "Enter any word:";
-        public const string showWordIsPalindrome = "Your word {0} is palindrome.";
-        public const string showWordNotPalindrome = "Your word {0} is NOT palindrome.";
+        public const string showWrongWord = "Invalid input. Word contains digits.";
+        public const string showWordIsPalindrome = "Your word '{0}' is palindrome.";
+        public const string showWordNotPalindrome = "Your word '{0}' is NOT palindrome.";
     }
 }
